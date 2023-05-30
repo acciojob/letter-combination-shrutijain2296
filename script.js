483d46da4a81108ecf5f75b52319577bf33e769f
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-30 15:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	var mapping = {
+		  2: "abc",
+		  3: "def",
+		  4: "ghi",
+		  5: "jkl",
+		  6: "mno",
+		  7: "pqrs",
+		  8: "tuv",
+		  9: "wxyz",
+		  0: "0",
+		  1: "1"
+	};
+
+	var stringArray = [];
+	
 }
 
 module.exports = letterCombinations;
